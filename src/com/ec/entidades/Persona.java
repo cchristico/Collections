7bd75e22949ec4cedbb97694cb875f4a23cb1ab8
Christian Castillo <cchristico@gmail.com>
2017-01-05 18:23:36 -0500

    Revert "Sobrescritura de toString"
    This reverts commit 71afd5828ac8abf3fbcbdd1fba89a86c83da0ce6.

--- src/com/ec/entidades/Persona.java
+++ src/com/ec/entidades/Persona.java
@@ -19,9 +19,16 @@ public class Persona {
         this.apellido = apellido;
         this.edad = edad;
     }
+    public Persona( ) {
+     
+    }
+    
+    
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -41,9 +48,6 @@ public class Persona {
     public void setEdad(int edad) {
         this.edad = edad;
     }
-    @Override
-    public String toString(){
-        return this.getNombre()+" "+this.getApellido()+" "+this.edad;
-    }
+    
     
 }
