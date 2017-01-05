5e6e6cb8446a2a5c4979430fc986a5f9c1e7a6aa
Christian Castillo <cchristico@gmail.com>
2017-01-05 16:51:13 -0500

    Agregar elementos a un arreglo de strings

--- src/com/ec/vistas/main.java
+++ src/com/ec/vistas/main.java
@@ -16,6 +16,15 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Arreglo de strings
+        String personas[]= new String[5];
+        System.out.println(personas.length);
+        personas[0]="Manuel";
+        personas[1]="Maria";
+        personas[2]="Juana";
+        personas[3]="Pepina";
+        personas[4]="Dulcinea";
+        System.out.println(personas[0]+"\n"+personas[1]+"\n"+personas[2]+"\n"+personas[3]+"\n"+personas[4]);
     }
     
 }
